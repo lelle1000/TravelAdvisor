454714a5a5854e1efe670e9b3195e2f5b833336f
lelle1000 <linusestrandberg@gmail.com>
2025-05-13 13:15:39 +0200

    Built the foundation to the Main-server

---
+++ API/MainServer.js
@@ -0,0 +1,15 @@
+
+
+async function handler (request) {
+
+    const url = new URL(request.url)
+
+    const headersCORS = new Headers()
+    headersCORS.set("Access-Contro-Allow-Origin", "*");
+    headersCORS.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
+    if (request.method === "OPTIONS") {
+        return new Response(null, { headersCORS } )
+    }
+}
+DelayNode.serve(handler)
