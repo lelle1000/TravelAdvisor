8ee67b7fd3c88ab52147ebbc7eb18852067bd1cc
Linus Strandberg <linusestrandberg@gmail.com>
2025-05-21 01:12:50 +0200

    Created a fetch system that fetches a capital based on a country and a photo to said country

---
+++ Webbsida/klient.js
@@ -0,0 +1,33 @@
+const UnsplashKey = "RXfEp3EulaHn3LgZG-m4BEel7MWwBee2iFESNQ7eLoc"
+
+async function getCapitalPhoto (capitalName) { // hämtar en bild till staden
+    const url = `https://api.unsplash.com/search/photos?query=${capitalName}&client_id=${UnsplashKey}`;
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if(data.results & data.results.length > 0) {
+        return data.results.urls.small
+    }
+    return null;
+    
+}
+getCapitalPhoto("Stockholm")
+
+async function fetchCountryAndPhoto() { // skriver ut staden baserat på land och en url med en bild på staden.
+    const CountryResponse = await fetch("https://restcountries.com/v3.1/capital/stockholm")
+    const CountryData = await CountryResponse.json()
+
+    if (CountryData.length > 0) {
+        const capital = countries[0].capital[0]
+        const PhotoUrl = await getCapitalPhoto(capital)
+
+        if (PhotoUrl) {
+            return capital, PhotoUrl
+        } else {
+            return null
+        }
+    }
+}
+
+fetchCountryAndPhoto()
