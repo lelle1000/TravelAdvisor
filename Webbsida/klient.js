f9df6b725a80f8f2bf3299c10a14753dfcf0e80c
Linus Strandberg <linusestrandberg@gmail.com>
2025-05-21 13:57:22 +0200

    Quickfix

--- Webbsida/klient.js
+++ Webbsida/klient.js
@@ -1,9 +1,7 @@
 const UnsplashKey = "RXfEp3EulaHn3LgZG-m4BEel7MWwBee2iFESNQ7eLoc"
 
 async function getCapitalPhoto (capitalName) { // hämtar en bild till staden
-    const url = `https://api.unsplash.com/search/photos?query=${capitalName}&client_id=${UnsplashKey}`;
-
-    const response = await fetch(url)
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${capitalName}&client_id=${UnsplashKey}`)
     const data = await response.json()
 
     if(data.results & data.results.length > 0) {
