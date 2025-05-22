90519a9ef1947b53311c72127fdd96da9bcf49b8
philles85 <philster04@outlook.com>
2025-05-22 13:36:32 +0200

    Added the external api to server instead

--- API/MainServer.js
+++ API/MainServer.js
@@ -1,3 +1,25 @@
+const Countries = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+    "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+    "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+    "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Brazzaville)", "Congo (Kinshasa)",
+    "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador",
+    "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
+    "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
+    "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland",
+    "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait",
+    "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
+    "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico",
+    "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
+    "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman",
+    "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar",
+    "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "São Tomé and Príncipe", "Saudi Arabia",
+    "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+    "South Sudan", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan",
+    "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu",
+    "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela",
+    "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+]
 
 const homePageRoute = new URLPattern({ pathname: "/homepage" });
 const homePageSigninRoute = new URLPattern({ pathname: "/homepage/signin" });
@@ -16,15 +38,56 @@ async function handler(request) {
     const infoPageMatch = informationPageRoute.exec(url);
 
     const headersCORS = new Headers()
-    headersCORS.set("Access-Contro-Allow-Origin", "*");
+    headersCORS.set("Access-Control-Allow-Origin", "*");
     headersCORS.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
     headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
     if (request.method === "OPTIONS") {
-        return new Response(null, { headersCORS })
+        return new Response(null, { status: 204, headers: headersCORS })
     }
 
 
     if (homePageMatch) {
+
+        if (request.method == "GET") {
+            const UnsplashKey = "RXfEp3EulaHn3LgZG-m4BEel7MWwBee2iFESNQ7eLoc"
+
+            async function getCapitalPhoto(capitalName) { // hämtar en bild till staden
+                const response = await fetch(`https://api.unsplash.com/search/photos?query=${capitalName}&client_id=${UnsplashKey}`)
+                const data = await response.json()
+
+                if (data.results && data.results.length > 0) {
+                    return data.results[0].urls.small;
+                }
+                return null;
+
+            }
+
+            async function fetchCountryAndPhoto(Country) { // skriver ut staden baserat på land och en url med en bild på staden.
+                const CountryResponse = await fetch(`https://restcountries.com/v3.1/name/${Country}`)
+                const CountryData = await CountryResponse.json()
+
+                if (CountryData.length > 0) {
+                    const capital = CountryData[0].capital[0]
+                    const PhotoUrl = await getCapitalPhoto(capital)
+
+                    if (PhotoUrl) {
+                        return { url: PhotoUrl, countryname: capital };
+
+                    } else {
+                        return null
+                    }
+
+                }
+
+            }
+
+            let RandomCountry = Countries[Math.floor(Countries.length * Math.random())]
+            const result = await fetchCountryAndPhoto(RandomCountry);
+
+            return new Response(JSON.stringify(result), {
+                headers: headersCORS,
+            });
+        }
 
     }
 
